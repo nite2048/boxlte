8ecfc585ff70cfa3b812e6d6d75bd21a358dc654
nite2048 <nite2048@gmail.com>
2024-02-04 18:26:28 +0530

    1. - Added files

---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+export { default as World } from './World.svelte'
+export { default as Light } from './Light.svelte'
+export { default as Empty } from './Empty.svelte'
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {Light, World} from "../lib/index.js";
+    import {Box, Plane} from "../lib/shapes/index.js";
+</script>
+
+<World cameraPosition={{y: 2, z: 5}} centerFocus>
+    <Light position={{x: 1, y: 5, z: 4}} intensity={Math.PI * 13} spot/>
+
+    <Box color="deepskyblue" position={{y: 0.5}}/>
+    <Plane color="white" radius={3} rounded alignHorizontal noShadowCast />
+</World>
+
